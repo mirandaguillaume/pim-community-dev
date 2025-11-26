a511be67e32f5e9c231199a89133349f61171dbb
Guillaume MIRANDA <guillaume.miranda@consulting-for.edenred.com>
2025-11-26 09:58:42 +0100

    Try for GHA

--- docker/wait_docker_up.sh
+++ docker/wait_docker_up.sh
@@ -2,30 +2,25 @@
 
 set -e
 
-MAX_COUNTER=45
-COUNTER=1
+DOCKER_COMPOSE_CMD=${DOCKER_COMPOSE:-$(command -v docker-compose >/dev/null 2>&1 && echo docker-compose || echo "docker compose")}
+MAX_COUNTER=${WAIT_FOR_MAX_COUNTER:-120}
 
-echo "Waiting for MySQL server…"
-while ! docker-compose exec mysql mysql --protocol TCP -uroot -proot -e "show databases;" > /dev/null 2>&1; do
-    COUNTER=$((${COUNTER} + 1))
-    if [ ${COUNTER} -gt ${MAX_COUNTER} ]; then
-        echo "We have been waiting for MySQL too long already; failing." >&2
-        exit 1
-    fi;
-    sleep 1
-done
+wait_for() {
+    local service=$1
+    local check_command=$2
+    local counter=1
 
-echo "MySQL server is running!"
+    echo "Waiting for ${service} server…"
+    while ! ${DOCKER_COMPOSE_CMD} exec ${service} sh -c "${check_command}" > /dev/null 2>&1; do
+        counter=$((counter + 1))
+        if [ ${counter} -gt ${MAX_COUNTER} ]; then
+            echo "We have been waiting for ${service} too long already; failing." >&2
+            exit 1
+        fi;
+        sleep 1
+    done
+    echo "${service^} server is running!"
+}
 
-COUNTER=1
-echo "Waiting for Elasticsearch server…"
-while ! docker-compose exec elasticsearch curl -s -k --fail "http://elasticsearch:9200/_cluster/health?wait_for_status=yellow&timeout=1s" > /dev/null; do
-    COUNTER=$((${COUNTER} + 1))
-    if [ ${COUNTER} -gt ${MAX_COUNTER} ]; then
-        echo "We have been waiting for Elasticsearch too long already; failing." >&2
-        exit 1
-    fi;
-    sleep 1
-done
-
-echo "Elasticsearch server is running!"
+wait_for mysql "mysql --protocol TCP -uroot -proot -e 'show databases;'"
+wait_for elasticsearch "curl -s -k --fail 'http://elasticsearch:9200/_cluster/health?wait_for_status=yellow&timeout=1s'"
