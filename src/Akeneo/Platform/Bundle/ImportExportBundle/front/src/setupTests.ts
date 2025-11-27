64d2919e7a98556a329eff0a55fe793784e6fafb
Guillaume MIRANDA <guillaume.miranda@consulting-for.edenred.com>
2025-11-27 17:18:00 +0100

    Fix setupTests fetch typing

--- src/Akeneo/Platform/Bundle/ImportExportBundle/front/src/setupTests.ts
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/front/src/setupTests.ts
@@ -1,14 +1,8 @@
 import '@testing-library/jest-dom/extend-expect';
 
-declare global {
-  // Allow mocking fetch in tests
-  // eslint-disable-next-line no-var
-  var fetch: typeof fetch;
-}
-
 beforeEach(() => {
   // Default no-op mock; individual tests can override as needed
-  global.fetch = jest.fn() as unknown as typeof fetch;
+  (global as unknown as { fetch: typeof fetch }).fetch = jest.fn() as unknown as typeof fetch;
 });
 
 export {};
