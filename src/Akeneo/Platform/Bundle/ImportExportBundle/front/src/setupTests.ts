58cbe566fce4b12ba584061ff846fda6b9276f34
mirandaguillaume <guillaume11miranda@gmail.com>
2025-11-28 11:17:41 +0100

    Add NodeJS.Global fetch typing for tests

--- src/Akeneo/Platform/Bundle/ImportExportBundle/front/src/setupTests.ts
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/front/src/setupTests.ts
@@ -1,8 +1,18 @@
 import '@testing-library/jest-dom/extend-expect';
 
+declare global {
+  // Allow NodeJS.Global to carry fetch for test mocks
+  // eslint-disable-next-line no-namespace
+  namespace NodeJS {
+    interface Global {
+      fetch: typeof fetch;
+    }
+  }
+}
+
 beforeEach(() => {
   // Default no-op mock; individual tests can override as needed
-  (globalThis as unknown as {fetch: typeof fetch}).fetch = jest.fn() as unknown as typeof fetch;
+  global.fetch = jest.fn() as unknown as typeof fetch;
 });
 
 export {};
