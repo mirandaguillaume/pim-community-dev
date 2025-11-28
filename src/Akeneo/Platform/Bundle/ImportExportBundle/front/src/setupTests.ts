5cc3660bb608db5803b47d15d66c4c3a48a9f762
mirandaguillaume <guillaume11miranda@gmail.com>
2025-11-28 12:21:36 +0100

    Remove eslint disable comment in setupTests

--- src/Akeneo/Platform/Bundle/ImportExportBundle/front/src/setupTests.ts
+++ src/Akeneo/Platform/Bundle/ImportExportBundle/front/src/setupTests.ts
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom/extend-expect';
 
 declare global {
   // Allow NodeJS.Global to carry fetch for test mocks
-  // eslint-disable-next-line no-namespace
   namespace NodeJS {
     interface Global {
       fetch: typeof fetch;
