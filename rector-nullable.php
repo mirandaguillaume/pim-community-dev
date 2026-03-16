42951916a4974c18837c2690414ca5ebf284fe67
Guillaume Miranda <guillaume11miranda@gmail.com>
2026-03-16 22:10:11 +0100

    chore: remove temporary rector config

--- rector-nullable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Rector\Config\RectorConfig;
-use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
-
-return RectorConfig::configure()
-    ->withPaths([__DIR__ . '/src', __DIR__ . '/components'])
-    ->withRules([ExplicitNullableParamTypeRector::class]);
