d6e8e892b3c133331a713cb1c93ced9df646f21a
mirandaguillaume <guillaume11miranda@gmail.com>
2026-04-13 11:18:15 +0200

    fix(deps): bump phpseclib/phpseclib from 3.0.50 to 3.0.51 (#142)
    * build(deps): bump phpseclib/phpseclib from 3.0.50 to 3.0.51
    
    Bumps [phpseclib/phpseclib](https://github.com/phpseclib/phpseclib) from 3.0.50 to 3.0.51.
    - [Release notes](https://github.com/phpseclib/phpseclib/releases)
    - [Changelog](https://github.com/phpseclib/phpseclib/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/phpseclib/phpseclib/compare/3.0.50...3.0.51)
    
    ---
    updated-dependencies:
    - dependency-name: phpseclib/phpseclib
      dependency-version: 3.0.51
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix(apps): cast openssl_config_path parameter to string for phpseclib 3.0.51
    
    phpseclib 3.0.51 tightened setOpenSSLConfigPath() to expect string.
    ContainerInterface::getParameter() returns array|bool|float|int|string|UnitEnum|null
    (Symfony 6.4), so an explicit cast is required.
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- upgrades/schema/Version_6_0_20211214000000_add_openid_keys_into_pim_configuration.php
+++ upgrades/schema/Version_6_0_20211214000000_add_openid_keys_into_pim_configuration.php
@@ -64,6 +64,7 @@ class Version_6_0_20211214000000_add_openid_keys_into_pim_configuration extends 
          * see http://phpseclib.sourceforge.net/x509/guide.html#selfsigned
          */
         $openSSLConfigPath = $this->container->getParameter('openssl_config_path');
+        \assert(\is_string($openSSLConfigPath));
         RSA::setOpenSSLConfigPath($openSSLConfigPath);
         /** @var RSA\PrivateKey $privateKey */
         $privateKey = RSA::createKey();
