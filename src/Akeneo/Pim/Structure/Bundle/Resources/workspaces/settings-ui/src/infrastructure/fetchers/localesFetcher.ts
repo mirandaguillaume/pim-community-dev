111238815d9951423ab3aed665d3fcd776c1cf5a
Guillaume Miranda <guillaume11miranda@gmail.com>
2026-04-27 19:31:30 +0200

    fix(lint): suppress pre-existing @typescript-eslint any warnings on legacy FetcherRegistry require

--- src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/infrastructure/fetchers/localesFetcher.ts
+++ src/Akeneo/Pim/Structure/Bundle/Resources/workspaces/settings-ui/src/infrastructure/fetchers/localesFetcher.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires,
+   @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access,
+   @typescript-eslint/no-unsafe-return */
 import {Locale} from '../../models';
 
 const FetcherRegistry = require('pim/fetcher-registry');
