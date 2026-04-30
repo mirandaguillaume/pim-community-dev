fa7d4285711673fbbc36dbd9535ff048fb083676
Guillaume Miranda <guillaume11miranda@gmail.com>
2026-04-30 15:21:49 +0200

    fix(test): import @testing-library/jest-dom in SecurityGuard and Translate tests

--- src/Akeneo/Connectivity/Connection/front/tests/src/shared/security/SecurityGuard.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/shared/security/SecurityGuard.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 import {SecurityGuard} from '@src/shared/security/SecurityGuard';
 import {SecurityContext} from '@src/shared/security/security-context';
--- src/Akeneo/Connectivity/Connection/front/tests/src/shared/translate/Translate.test.tsx
+++ src/Akeneo/Connectivity/Connection/front/tests/src/shared/translate/Translate.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/react';
 import {Translate} from '@src/shared/translate/Translate';
 import {TranslateContext} from '@src/shared/translate/translate-context';
