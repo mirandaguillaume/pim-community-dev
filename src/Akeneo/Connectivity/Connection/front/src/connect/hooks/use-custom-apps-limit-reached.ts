947d420fdb3773663ccd0d9dfabed116d9672d71
Guillaume Miranda <guillaume11miranda@gmail.com>
2026-05-05 21:57:28 +0200

    fix(connectivity): migrate useCustomAppsLimitReached to @tanstack/react-query v5 object syntax

--- src/Akeneo/Connectivity/Connection/front/src/connect/hooks/use-custom-apps-limit-reached.ts
+++ src/Akeneo/Connectivity/Connection/front/src/connect/hooks/use-custom-apps-limit-reached.ts
@@ -12,13 +12,16 @@ type Result = {
 export const useCustomAppsLimitReached = (): Result => {
     const url = useRoute('akeneo_connectivity_connection_custom_apps_rest_max_limit_reached');
 
-    return useQuery<boolean, Error, boolean>(['custom-apps-limit-reached'], async () => {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-            },
-        });
-        return await response.json();
+    return useQuery<boolean, Error, boolean>({
+        queryKey: ['custom-apps-limit-reached'],
+        queryFn: async () => {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                },
+            });
+            return await response.json();
+        },
     });
 };
