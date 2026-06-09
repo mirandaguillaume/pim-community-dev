56e7abc185465436de771637ddda625db33d6012
Guillaume Miranda <guillaume11miranda@gmail.com>
2026-06-09 15:35:55 +0200

    test(dqi): Criterion/Icon + Criterion/Title render tests

---
+++ src/Akeneo/Pim/Automation/DataQualityInsights/tests/front/unit/Application/component/ProductEditForm/TabContent/DataQualityInsights/Criterion/Icon.unit.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {DependenciesProvider} from '@akeneo-pim-community/legacy-bridge';
+import {ThemeProvider} from 'styled-components';
+import {pimTheme} from 'akeneo-design-system';
+import {Icon} from '../../../../../../../../../front/src/application/component/ProductEditForm/TabContent/DataQualityInsights/Criterion/Icon';
+
+const renderWith = (ui: React.ReactElement) =>
+  render(
+    <DependenciesProvider>
+      <ThemeProvider theme={pimTheme}>{ui}</ThemeProvider>
+    </DependenciesProvider>
+  );
+
+describe('Icon', () => {
+  it('clones child element with width and height 20', () => {
+    const {container} = renderWith(
+      <Icon type="svg">
+        <svg data-testid="inner-icon" />
+      </Icon>
+    );
+    const svg = container.querySelector('svg');
+    expect(svg).toHaveAttribute('width', '20');
+    expect(svg).toHaveAttribute('height', '20');
+  });
+
+  it('renders without children', () => {
+    const {container} = renderWith(<Icon type="svg" />);
+    expect(container.querySelector('span')).toBeInTheDocument();
+  });
+});
